ce9b6e18235d9524175cea45b0b3e08c273e4f65
lelouch3201 <charleskeane3201@gmail.com>
2023-06-30 15:12:51 +0800

    add menu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  menuItems = [
+    { title: 'View', path: '/view', icon: 'eye' },
+    { title: 'Update', path: '/update', icon: 'create' },
+    { title: 'View Record', path: '/view-record', icon: 'list' }
+  ];
 
   constructor() {}
-
 }
